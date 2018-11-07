2f07475f52c965fd22152d2f991f82d9171a6f29
yu71941628 <zhengyuan.beihang@gmail.com>
2018-11-07 15:51:20 -0600

    setup.sh change

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
 # setup tensorflow and char-rnn in AWS ec2 ubuntu machine.
-apt-get install python
-wget https://bootstrap.pypa.io/get-pip.py
-python get-pip.py
-apt-get update
-apt-get upgrade
-apt-get install git
-pip install --upgrade tensorflow
-git clone https://github.com/yu71941628/EECS349_hw3_part2.git
+sudo apt update
+sudo apt install python-setuptools
+sudo apt-get install python3-pip
+pip3 install --upgrade tensorflow
+git clone https://github.com/yu71941628/tensorflow-char-rnn.git
 cd tensorflow-char-rnn
